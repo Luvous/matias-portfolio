2e18792408187b1cbd35d7d1236946ffa02c5044
Luvous <thomasvarela2001@outlook.es>
2021-11-10 16:41:37 -0300

    First commitment, whole project

---
+++ app.js
@@ -0,0 +1,339 @@
+require('dotenv').config();
+const express = require ('express');
+const ejs = require('ejs');
+const app = express();
+const port = 3000;
+const mongoose = require('mongoose');
+const cloudinary = require('cloudinary').v2;
+const _ = require('lodash');
+const multer  = require('multer');
+const path = require('path');
+const connectEnsureLogin = require('connect-ensure-login');
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+
+cloudinary.config({
+  cloud_name: process.env.cloud_name,
+  api_key: process.env.api_key,
+  api_secret: process.env.api_secret
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "MatiasVarela"
+  }
+});
+
+const upload = multer({ storage: storage });
+
+
+/*  PASSPORT SETUP  ----------------------------------------------------------*/
+
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const UserDetail = new mongoose.Schema({
+  username: String,
+  password: String
+});
+
+UserDetail.plugin(passportLocalMongoose);
+const UserDetails = mongoose.model('userInfo', UserDetail, 'userInfo');
+
+
+const expressSession = require('express-session')({
+  secret: process.env.expressSession_secret,
+  resave: false,
+  saveUninitialized: false
+});
+
+/* PASSPORT LOCAL AUTHENTICATION -----------------------------------------*/
+
+passport.use(UserDetails.createStrategy());
+
+passport.serializeUser(UserDetails.serializeUser());
+passport.deserializeUser(UserDetails.deserializeUser());
+
+app.use(expressSession);
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// -------------------------------------------------------------------------
+
+
+/* ROUTES */
+
+
+
+app.post('/login', (req, res, next) => {
+  passport.authenticate('local',
+  (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.redirect('/login?info=' + _.kebabCase(info));
+    }
+    req.logIn(user, function(err) {
+      if (err) {
+        return next(err);
+      }
+      return res.redirect('/nAdmin');
+    });
+
+  })(req, res, next);
+});
+
+app.get('/login',
+  (req, res) => res.render('login')
+);
+
+
+
+
+// --------------------------------------------------------------------------
+
+// MongoDB Connection
+mongoose.connect('mongodb://localhost:27017/matiasDB');
+
+// Posts Schema
+const postSchema = {
+  img: String,
+  title: String,
+  description: String,
+  body: String,
+  date: String,
+  nLink: String
+}
+const Post = mongoose.model('post', postSchema);
+
+// Product Schema
+const productSchema = {
+  img: String,
+  title: String,
+  description: String,
+  body: String,
+  flashcookieLink: String
+}
+const Product = mongoose.model('product', productSchema);
+
+// Items Schema for portfolio
+const itemSchema = {
+  img: String
+};
+
+const Item = mongoose.model('item', itemSchema);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname+'/index.html');
+});
+
+app.get('/about', function(req, res){
+  res.render('about', {active:"contact", roller:"none"});
+});
+
+app.get('/contact', function(req, res){
+  res.render('contact', {active:"contact", roller:"none"});
+});
+
+app.get('/nAdmin', connectEnsureLogin.ensureLoggedIn(), function(req,res){
+    res.render('nAdmin', {active:"nAdmin", roller:"admin-dash"});
+})
+
+// Blog and posts -------------------------------------------------------------
+
+let posts = []
+
+app.get('/nAdmin/nNewPost', connectEnsureLogin.ensureLoggedIn(), function(req,res){
+    res.render('compose', {active:"nCompose", roller:"admin-dash", edit:"blog"});
+})
+
+let today = new Date();
+let options = {day: 'numeric', month: 'numeric', year: 'numeric'};
+let postDate = today.toLocaleDateString('en-GB', options);
+
+app.post('/nAdmin/nNewPost', upload.single('image'), function(req,res){
+  const post = new Post({
+    img: req.file.path,
+    title: req.body.postTitle,
+    description: req.body.postPreview,
+    body: req.body.postBody,
+    date: postDate,
+    nLink: _.kebabCase(req.body.postTitle)
+  });
+
+  post.save();
+  res.redirect('/blog');
+});
+
+app.get('/blog', function(req, res){
+  Post.find({}, function(err,foundPosts){
+    if (foundPosts.length == 0){
+      res.render('blog', {active:"blog", posts:posts, roller:"none"});
+    }else{
+      if (!err){
+        res.render('blog', {active:"blog", posts:foundPosts, roller:"none"});
+      }
+    }
+  });
+});
+
+app.get('/blog/:postURL', function(req, res){
+  Post.findOne({ nLink: _.kebabCase(req.params.postURL)}, function(err, foundPost){
+    if(!err){
+      res.render('post', {img:foundPost.img, title:foundPost.title, body:foundPost.body, active:"post", roller:"admin-dash", date:foundPost.date})
+    } else {
+      res.redirect('/blog')
+    }
+  })
+})
+
+// Deleting posts from blog-
+
+app.get('/nAdmin/nDeletePost', connectEnsureLogin.ensureLoggedIn(), function(req,res){
+  Post.find({}, function(err,foundPosts){
+    if(!err){
+      res.render('remove', {active:"remove-post", posts:foundPosts, roller:'admin-dash'});
+    }else{
+      console.log(err);
+    }
+  });
+});
+
+app.post('/nAdmin/nDeletePost', connectEnsureLogin.ensureLoggedIn(), function(req,res){
+  const checkPostId = req.body.checkbox;
+
+  Post.findByIdAndRemove(checkPostId, function(err){
+    if(err){
+      console.log(err);
+    } else {
+      res.redirect('/nAdmin/nDeletePost')
+    }
+  })
+});
+
+
+//----------------------------------Store and products-----------------------------
+
+let products = [];
+
+app.get('/nAdmin/nNewProduct', connectEnsureLogin.ensureLoggedIn(), function(req,res){
+  res.render('compose', {active:"nCompose", roller:"admin-dash", edit:"store"})
+});
+
+app.post('/nAdmin/nNewProduct', upload.single('image'), function(req,res){
+
+  const product = new Product ({
+    img: req.file.path,
+    title: req.body.postTitle,
+    description: req.body.postPreview,
+    body: req.body.postBody,
+    flashcookieLink: req.body.flashLink
+  });
+
+  product.save();
+  res.redirect('/shop');
+})
+
+
+app.get('/shop', function(req, res){
+  Product.find({}, function(err,foundProducts){
+    if (foundProducts.length == 0){
+      res.render('shop', {active:"shop", products:products, roller:"none"});
+    }else{
+      if (!err){
+        res.render('shop', {active:"shop", products:foundProducts, roller:"none"});
+      }
+    }
+  })
+});
+
+
+
+// Deleting products from database--.-.-.-...-...-.-.-.-.-.--.-.-.-..
+
+app.get('/nAdmin/nDeleteProduct', connectEnsureLogin.ensureLoggedIn(), function(req,res){
+  Product.find({}, function(err,foundProducts){
+    if(!err){
+      res.render('remove', {active:"remove-product", products:foundProducts, roller:'admin-dash'});
+    }else{
+      console.log(err);
+    }
+  });
+});
+
+app.post('/nAdmin/nDeleteProduct', connectEnsureLogin.ensureLoggedIn(), function(req,res){
+  const checkProductId = req.body.checkbox;
+
+  Product.findByIdAndRemove(checkProductId, function(err){
+    if(err){
+      console.log(err);
+    } else {
+      res.redirect('/nAdmin/nDeleteProduct')
+    }
+  })
+});
+
+
+//---------Portfolio-----------------------------------------------------------
+
+let items = [];
+
+app.get('/nAdmin/nNewItem', connectEnsureLogin.ensureLoggedIn(), function(req,res){
+  res.render('compose', {active:"nCompose", roller:"admin-dash", edit:"portfolio"})
+});
+
+app.post('/nAdmin/nNewItem', upload.single('image'), function(req,res){
+
+  const item = new Item({
+    img: req.file.path
+  });
+
+  item.save();
+  res.redirect('/portfolio');
+})
+
+app.get('/portfolio', function(req, res){
+  Item.find({}, function(err, foundItems){
+    if (foundItems == 0){
+      res.render('portfolio', {active:"portfolio", roller:"none", items:items})
+    } else {
+      if (!err){
+        res.render('portfolio', {active:"portfolio", roller:"none", items:foundItems});
+      }
+    }
+  })
+});
+
+app.get('/nAdmin/nDeleteItem', connectEnsureLogin.ensureLoggedIn(), function(req,res){
+  Item.find({}, function(err,foundItems){
+    if(!err){
+      res.render('remove', {active:"remove-item", items:foundItems, roller:'admin-dash'});
+    }else{
+      console.log(err);
+    }
+  });
+});
+
+app.post('/nAdmin/nDeleteItem', connectEnsureLogin.ensureLoggedIn(), function(req,res){
+  const checkItemId = req.body.checkbox;
+
+  Item.findByIdAndRemove(checkItemId, function(err){
+    if(err){
+      console.log(err);
+    } else {
+      res.redirect('/nAdmin/nDeleteItem')
+    }
+  })
+});
+
+
+app.listen(process.env.PORT || port,function(){
+  console.log('Server is running, like Forrest but faster')
+});
---
+++ public/animations.js
@@ -0,0 +1,221 @@
+$(document).ready(function() {
+  $('.pop-box svg').click(function() {
+    $('.pop-box').removeClass('hovpop');
+    $('.pop-box').stop().animate({
+      height: 0,
+      opacity: 0,
+      bottom: '+=300'
+    }, 300);
+    $('.newsletter-box').stop().animate({
+      bottom: '+=55vh',
+      right:'1rem',
+    },350).show();
+    $('.newsletter-box').css('display', 'flex');
+  });
+
+
+  $('.right-nw svg').click(function() {
+      $('.pop-box').stop().animate({
+        height: '4rem',
+        opacity: 1,
+        bottom: '1.5rem'
+      }, 500);
+      setTimeout(function(){
+        $('.newsletter-box').css('display', 'none');
+      },320);
+
+      $('.newsletter-box').stop().animate({
+        bottom: "-50vh",
+        right: '10rem',
+      },400);
+      setTimeout( function(){
+        $('newsletter-box').hide()
+      }, 500);
+      setTimeout(function(){
+        $('.pop-box').addClass('hovpop')
+      }, 600);
+  });
+
+
+  $('.right-arrow').click(function() {
+    $('.nav').stop().animate({
+      left: '-1px'
+    },450);
+    $('.pullout').stop().animate({
+      width: '60px'
+    },700);
+    $('.nav-item').stop().animate({
+      marginTop: '20px',
+      marginBottom: '20px'
+    },400);
+    $('.gohome svg').stop().animate({
+      width: 0
+    },250);
+    if ($(window).width() < 640){
+      $('body').css('overflow-y', 'hidden')
+    }
+  });
+  
+  $('.pullout').click(function() {
+    $('.nav').stop().animate({
+      left: '-100vw'
+    },500)
+    $('.pullout').stop().animate({
+      width: '0'
+    },700)
+    $('.nav-item').stop().animate({
+      marginTop: '0',
+      marginBottom: '0'
+    },200)
+    $('.gohome svg').stop().animate({
+      width: '75px'
+    },300);
+    if ($(window).width() < 640){
+      $('body').css('overflow-y', 'auto')
+    }
+  });
+
+  $('li img').click(function(){
+    $('li').stop().animate({
+      height: '20vh'
+    },420)
+    linkVar = event.path[0].currentSrc;
+
+    $('.shop-card').animate({
+      opacity: '100%',
+      height: '70vh'
+    },200)
+
+    window.scrollTo({top: 70, behavior: 'smooth'});
+
+    $('.fullscreen').animate({
+      height: '100%',
+      opacity: '100%'
+    },200)
+
+    $('.fullscreen img').animate({
+      height: '100%',
+      opacity: '100%'
+    },300)
+    $('.fullscreen').css('display', 'flex');
+    $('.fullscreen img').attr('src', linkVar);
+
+    let liTitle = $(this).next();
+    $('.shop-card h2').text(liTitle.text());
+
+
+    $('.shop-item-size').text($(this).nextAll().eq(1).text());
+    $('.shop-item-description').text($(this).nextAll().eq(2).text());
+
+    $('.shop-item-info a').attr('href', $(this).nextAll().eq(3).text())
+  })
+
+  $('.cross-port').click(function(){
+    $('li').stop().animate({
+      height: '40vh'
+    },350)
+    $('.fullscreen').animate({
+      height: 0,
+      opacity: '0%'
+    },300);
+    $('.fullscreen img').animate({
+      height: 0,
+      opacity: '0%'
+    },210)
+    setTimeout(function(){
+      $('.fullscreen').css('display', 'none');
+      $('li').stop().animate({
+        height: '40vh'
+      },350);
+    },500);
+  })
+
+
+  $('.cross-shop').click(function(){
+    $('.shop-card').animate({
+      height: 0,
+      opacity: '0%'
+    },300);
+    setTimeout(function(){
+      $('.fullscreen').css('display', 'none');
+      $('li').stop().animate({
+        height: '40vh'
+      },350);
+    },250);
+  })
+
+  $(window).resize(function(){
+    if ($(window).width() > 640){
+      $(window).scrollTop(0,0);
+    }
+  })
+
+  let checkNavMob = false;
+
+  $('.undler').click(function(){
+    $(window).scrollTop(0,0);
+    if (checkNavMob === false){
+      checkNavMob = true;
+      $('.undler svg').addClass('animatedBurger');
+      $('.cross-navmob').removeClass('animatedBurger');
+      $('body').css('overflow', 'hidden');
+      $('.nav-items-mob .nav-item').stop().animate({
+        marginTop: '4rem'
+      },200);
+      $('.nav-items-mob .active').stop().animate({
+        marginTop: '4rem'
+      },200);
+      $('.nav-items-mob .active h4').stop().animate({
+        fontSize: '3rem'
+      },200);
+      $('.nav-items-mob .nav-item h4').stop().animate({
+        fontSize: '3rem'
+      },200);
+      $('.nav-items-mob').css('display', 'flex');
+      $('.nav-items-mob').css('flex-direction', 'column');
+      $('.nav-items-mob').css('justify-content', 'center');
+      $('.nav-items-mob').stop().animate({
+        height: '100vh'
+      },350);
+    } else {
+      checkNavMob = false;
+      $('.undler svg').removeClass('animatedBurger');
+      $('.cross-navmob').addClass('animatedBurger');
+      $('body').css('overflow', 'auto');
+      $('.nav-items-mob .active h4').stop().animate({
+        fontSize: '0'
+      },250);
+      $('.nav-items-mob .nav-item h4').stop().animate({
+        fontSize: '0'
+      },250);
+      setTimeout(function(){
+        $('.nav-items-mob').css('display', 'none');
+        $('.nav-items-mob').css('flex-direction', 'row');
+        $('.nav-items-mob').css('justify-content', 'center');
+      },250)
+      $('.nav-items-mob .nav-item').stop().animate({
+        marginTop: 0
+      },20);
+      $('.nav-items-mob .active').stop().animate({
+        marginTop: 0
+      },20);
+      $('.nav-items-mob').stop().animate({
+        height: '10vh'
+      },250)
+    }
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
